3a83943fa8f22ce141eb1d9623fe963568de7420
Gustavo <dsgustavo2020@gmail.com>
2023-08-23 10:01:16 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from  './componentes/Banner/Banner';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
