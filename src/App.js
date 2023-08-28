60aaa0639f32710b6ce82071b9235525b30cd712
Gustavo <gustavodossantos1704@gmail.com>
2023-08-28 19:18:51 -0300

    create the "time" component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import Banner from  './componentes/Banner';
 import Forumulario from './componentes/Formulario';
+import Time from './componentes/Time';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores([...colaboradores, colaborador])
+  }
+  
+  
+
   return (
     <div className="App">
       <Banner/>
-      <Forumulario/>
+      <Forumulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Front-end"/>
+      <Time nome="Data Science"/>
+      <Time nome="Dev-ops"/>
+      <Time nome="UX e Design"/>
+      <Time nome="Mobile"/>
+      <Time nome="Inovação e Gestão"/>
+
     </div>
   );
 }
