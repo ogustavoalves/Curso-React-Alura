6a0c48c5cafc26d88d0b7a513ded7ef595dd2123
Gustavo <gustavodossantos1704@gmail.com>
2023-08-30 15:27:46 -0300

    finished the footer

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,14 +3,16 @@ import './Rodape.css'
 const Rodape = () => {
     return (
         <footer className='rodape'>
+            <div className='fundo'></div>
             <div className='imgs'>
                 <div className='social'>
                     <img src='/imgs/fb.png' alt='facebook logo'/>
-                    <img src='/imgs/ig.png' alt='instagram logo'/>
                     <img src='/imgs/tw.png' alt='twitter logo'/>
+                    <img src='/imgs/ig.png' alt='instagram logo'/>
                 </div>
                 <div className='logo'><img src='/imgs/logo.png' alt='facebook logo'/></div>
             </div>
+            
         </footer>
     )
 }
